e459777d4be892912b5fb11cfc33379882ec1548
Dakota States <dakota.states@gmail.com>
2020-02-27 17:25:51 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,6 +1,4 @@
-class PartnerError < StandardError
-  
-end
+
 
 class Person
   attr_accessor :partner, :name
@@ -14,7 +12,15 @@ class Person
     person.partner = self
   end
 
+
+class PartnerError < StandardError
+  
 end
+
+
+end
+
+
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
